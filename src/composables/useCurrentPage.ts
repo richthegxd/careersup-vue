0bbb15e2753eb7c740df465f2c2a6fd2d1be6048
Артем Артемов <richthegxd@mail.ru>
2022-09-20 14:28:35 +0300

    Default page number in useCurrentPage composable

--- src/composables/useCurrentPage.ts
+++ src/composables/useCurrentPage.ts
@@ -3,7 +3,7 @@ import { useRoute } from "vue-router";
 
 const useCurrentPage = () => {
   const route = useRoute();
-  return computed(() => Number.parseInt(route.query.page as string));
+  return computed(() => Number.parseInt((route.query.page as string) || "1"));
 };
 
 export default useCurrentPage;
